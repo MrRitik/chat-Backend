b732b934a2e1a140299ace06851f1dc91a2de976
Ritik Kumar Singh <ritikkrsingh1474@gmail.com>
2024-05-01 19:14:28 +0530

    cors options

--- constants/config.js
+++ constants/config.js
@@ -2,7 +2,7 @@ const corsOptions = {
   origin: [
     "http://localhost:5173",
     "http://localhost:4173",
-    process.env.CLIENT_URL,
+    'https://chat-frontend-6l34e73uh-ritik-kumar-singhs-projects-cb9563b3.vercel.app',
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
